dde318d96699dee8c58d16d08077737adf86326d
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-06 21:59:46 -0500

    Create Walkspeed.lua

---
+++ Sources/Walkspeed.lua
@@ -0,0 +1,25 @@
+mouse = game.Players.LocalPlayer:GetMouse()
+mouse.KeyDown:Connect(function(key)
+    if key == getgenv().Key then
+safedie = true
+end
+end)
+
+mouse.KeyUp:Connect(function(key)
+    if key == getgenv().Key then
+safedie = false
+end
+end)
+
+
+
+	delay(0, function()
+	pcall(function()
+	game:GetService("RunService").stepped:connect(function()
+			if safedie == true then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,-1.6)
+
+		end
+		end)
+		end)
+end)
