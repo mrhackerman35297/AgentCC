d7697d8ed54394ccd0e66661fb72c24587ecb835
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-06 21:36:15 -0500

    Update and rename Bypass to Bypass.lua

---
+++ Sources/Bypass.lua
@@ -0,0 +1,16 @@
+--Feel Free to use this if you want
+function AntiCheat()
+  wait(1)
+  local me        = game.Players.LocalPlayer
+  local mechar    = game.Players.LocalPlayer.Character
+  for _, v in next, mechar:GetChildren() do
+    if v:IsA('Script') then
+      for _, b in next, v:GetChildren() do
+        if b:IsA('LocalScript') and b.Parent:IsA('Script') then
+          b.Parent:Destroy()
+        end
+      end 
+    end
+  end 
+end
+game.Players.KicakOkayer.CharacterAdded:Connect(AntiCheat)
