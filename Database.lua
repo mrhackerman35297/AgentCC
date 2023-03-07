de10007ba350efcdf23fc1dc0449e219ec614d55
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-06 21:06:51 -0500

    Update Database.lua

--- Database.lua
+++ Database.lua
@@ -5,23 +5,33 @@ Database = {
   Status = "",
   Website = "https://9kdestroyer.xyz", -- Indevelopement
   Blacklist = {},
- 
+  Ranks = {
+    Superheros = {},
+    AbusePerms = {},
+    Premium = {},  
+  },
 }
 
 
 
-
+function Whitelist(Direction, id)
+    table.insert(Direction, id)
+end
+--[[
+Example:
+Whitelist(Database.Ranks.Superheros, 12345)
+]]
 
 function BlacklistUser(UserId)
 table.insert(Database.Blacklist, UserId)
 end
 --[[
-Exampe:
+Example:
 BlacklistUser(12345) -- Killing me
 ]]
+
 if table.find(Database.Blacklist, game:GetService("Players").LocalPlayer.UserId) then
     game:GetService("Players").LocalPlayer:Kick("Agent | You are Blacklisted")
 end
-
 
 return Database
