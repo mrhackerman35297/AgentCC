37b749b4777ba33d9560020e0314b72132325371
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-07 21:11:12 -0500

    Update Noclip.lua

--- Sources/Noclip.lua
+++ Sources/Noclip.lua
@@ -1,20 +1,57 @@
-
-game:service("RunService").Stepped:connect(function()
-    if getgenv().Noclip == true then
-        if game.Players.LocalPlayer.Character:FindFirstChild("BOOMBOXHANDLE") then
-            game.Players.LocalPlayer.Character:FindFirstChild("BOOMBOXHANDLE"):Destroy()
-            game.Players.LocalPlayer.Character:WaitForChild("UpperTorso").CanCollide = false
-            game.Players.LocalPlayer.Character:WaitForChild("LowerTorso").CanCollide = false
-            game.Players.LocalPlayer.Character:WaitForChild("Head").CanCollide = false
-            game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CanCollide = false
-        else
-            if game.Players.LocalPlayer.Character:FindFirstChild("In-gameMask") then
-                game.Players.LocalPlayer.Character:FindFirstChild("In-gameMask"):FindFirstChildOfClass("Model"):FindFirstChild("Head").CanCollide = false 
-            end
-            game.Players.LocalPlayer.Character:WaitForChild("UpperTorso").CanCollide = false
-            game.Players.LocalPlayer.Character:WaitForChild("LowerTorso").CanCollide = false
-            game.Players.LocalPlayer.Character:WaitForChild("Head").CanCollide = false
-            game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CanCollide = false
-        end
-    end
-end)
+xpcall(function()
+                    local old
+                    old = hookmetamethod(game, "__namecall" ,newcclosure(function(self,...)
+                        if not checkcaller() then return old(self,...) end
+                        local args = {...}
+            
+                        if self == PhysicsService then
+                            if getnamecallmethod() == "RenameCollisionGroup" then
+                                return	RenameCollisionGroup(args[1],args[2])
+                            elseif getnamecallmethod() == "RemoveCollisionGroup"  then
+                                return	RemoveCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CreateCollisionGroup" then
+                                return	CreateCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CollisionGroupSetCollidable" then
+                                return CollisionGroupSetCollidable(args[1],args[2],args[3])
+                            end
+                        end 
+            
+                        return old(self,...)
+                    end))
+                end, function()
+                    local mt = getrawmetatable(game)
+                    local old = mt.__namecall
+                    
+                    setreadonly(mt,false)
+                    
+                    mt.__namecall = newcclosure(function(self, ...)
+                        if not checkcaller() then return old(self,...) end
+                        local args = {...}
+            
+                        if self == PhysicsService then
+                            if getnamecallmethod() == "RenameCollisionGroup" then
+                                return	RenameCollisionGroup(args[1],args[2])
+                            elseif getnamecallmethod() == "RemoveCollisionGroup"  then
+                                return	RemoveCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CreateCollisionGroup" then
+                                return	CreateCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CollisionGroupSetCollidable" then
+                                return CollisionGroupSetCollidable(args[1],args[2],args[3])
+                            end
+                        end 
+            
+                        return old(self,...)
+                    end)
+            
+                    setreadonly(mt,true)
+                end)
+                local RunService = game:GetService('RunService')
+                RunService.Heartbeat:Connect(function(step)
+                    if getgenv().Noclip == true then
+                        game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+                    end
+                end)
+            end)();
+        end)
+        setfflag("HumanoidParallelRemoveNoPhysics", "False")
+        setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")      
