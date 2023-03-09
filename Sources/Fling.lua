46209f270e7708e54a6181024880def913daa91f
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-08 22:03:25 -0500

    Update Fling.lua

--- Sources/Fling.lua
+++ Sources/Fling.lua
@@ -1,3 +1,52 @@
+xpcall(function()
+                    local old
+                    old = hookmetamethod(game, "__namecall" ,newcclosure(function(self,...)
+                        if not checkcaller() then return old(self,...) end
+                        local args = {...}
+            
+                        if self == PhysicsService then
+                            if getnamecallmethod() == "RenameCollisionGroup" then
+                                return	RenameCollisionGroup(args[1],args[2])
+                            elseif getnamecallmethod() == "RemoveCollisionGroup"  then
+                                return	RemoveCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CreateCollisionGroup" then
+                                return	CreateCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CollisionGroupSetCollidable" then
+                                return CollisionGroupSetCollidable(args[1],args[2],args[3])
+                            end
+                        end 
+            
+                        return old(self,...)
+                    end))
+                end, function()
+                    local mt = getrawmetatable(game)
+                    local old = mt.__namecall
+                    
+                    setreadonly(mt,false)
+                    
+                    mt.__namecall = newcclosure(function(self, ...)
+                        if not checkcaller() then return old(self,...) end
+                        local args = {...}
+            
+                        if self == PhysicsService then
+                            if getnamecallmethod() == "RenameCollisionGroup" then
+                                return	RenameCollisionGroup(args[1],args[2])
+                            elseif getnamecallmethod() == "RemoveCollisionGroup"  then
+                                return	RemoveCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CreateCollisionGroup" then
+                                return	CreateCollisionGroup(args[1])
+                            elseif getnamecallmethod() == "CollisionGroupSetCollidable" then
+                                return CollisionGroupSetCollidable(args[1],args[2],args[3])
+                            end
+                        end 
+            
+                        return old(self,...)
+                    end)
+            
+                    setreadonly(mt,true)
+        end)
+        setfflag("HumanoidParallelRemoveNoPhysics", "False")
+        setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")      
 game:GetService("RunService").RenderStepped:Connect(function()
     	--// [Fling]
 	if getgenv().Fling == true then
