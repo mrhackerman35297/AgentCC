b38d8eef1389a16db18d23de7eefd3ebf12599cc
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-10 20:57:26 -0500

    Update Fling.lua

--- Sources/Fling.lua
+++ Sources/Fling.lua
@@ -1,80 +1,50 @@
-xpcall(function()
-                    local old
-                    old = hookmetamethod(game, "__namecall" ,newcclosure(function(self,...)
-                        if not checkcaller() then return old(self,...) end
-                        local args = {...}
-            
-                        if self == PhysicsService then
-                            if getnamecallmethod() == "RenameCollisionGroup" then
-                                return	RenameCollisionGroup(args[1],args[2])
-                            elseif getnamecallmethod() == "RemoveCollisionGroup"  then
-                                return	RemoveCollisionGroup(args[1])
-                            elseif getnamecallmethod() == "CreateCollisionGroup" then
-                                return	CreateCollisionGroup(args[1])
-                            elseif getnamecallmethod() == "CollisionGroupSetCollidable" then
-                                return CollisionGroupSetCollidable(args[1],args[2],args[3])
-                            end
-                        end 
-            
-                        return old(self,...)
-                    end))
-                end, function()
-                    local mt = getrawmetatable(game)
-                    local old = mt.__namecall
-                    
-                    setreadonly(mt,false)
-                    
-                    mt.__namecall = newcclosure(function(self, ...)
-                        if not checkcaller() then return old(self,...) end
-                        local args = {...}
-            
-                        if self == PhysicsService then
-                            if getnamecallmethod() == "RenameCollisionGroup" then
-                                return	RenameCollisionGroup(args[1],args[2])
-                            elseif getnamecallmethod() == "RemoveCollisionGroup"  then
-                                return	RemoveCollisionGroup(args[1])
-                            elseif getnamecallmethod() == "CreateCollisionGroup" then
-                                return	CreateCollisionGroup(args[1])
-                            elseif getnamecallmethod() == "CollisionGroupSetCollidable" then
-                                return CollisionGroupSetCollidable(args[1],args[2],args[3])
-                            end
-                        end 
-            
-                        return old(self,...)
-                    end)
-            
-                    setreadonly(mt,true)
-        end)
-        setfflag("HumanoidParallelRemoveNoPhysics", "False")
-        setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")      
-game:GetService("RunService").RenderStepped:Connect(function()
-    	--// [Fling]
-	if getgenv().Fling == true then
-		local FlingEnemy = game.Players[getgenv().FlingTarget].Character
-		if FlingEnemy then
-			FlingTorso = FlingEnemy.UpperTorso
-			local dis = 3.85
-			local increase = 6
-			if FlingEnemy.Humanoid.MoveDirection.X < 0 then
-				xchange = -increase
-			elseif FlingEnemy.Humanoid.MoveDirection.X > 0  then
-				xchange = increase
-			else
-				xchange = 0
-			end
-			if FlingEnemy.Humanoid.MoveDirection.Z < 0 then
-				zchange = -increase
-			elseif FlingEnemy.Humanoid.MoveDirection.Z > 0 then
-				zchange = increase
-			else
-				zchange = 0
-			end          
-			if game.Players.LocalPlayer.Character then
-				game.Players.LocalPlayer.Character:FindFirstChildWhichIsA('Humanoid'):ChangeState(11)
-				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(FlingTorso.Position.X + math.random(-dis,dis) + xchange, FlingTorso.Position.Y, FlingTorso.Position.Z + math.random(-dis,dis) + zchange) * CFrame.Angles(math.rad(game.Players.LocalPlayer.Character.HumanoidRootPart.Orientation.X + 350),math.rad(game.Players.LocalPlayer.Character.HumanoidRootPart.Orientation.Y + 200),math.rad(game.Players.LocalPlayer.Character.HumanoidRootPart.Orientation.Z + 240))
-				game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(500000,500000,500000)
-			end
-		end
-	end
-
-end)
+local Players = game.Players
+		local player = game.Players.LocalPlayer
+        local Loop
+        local OldFlingPos = player.Character.HumanoidRootPart.Position
+        local loopFunction = function()
+            local success,err = pcall(function()
+                local FlingEnemy = Players:FindFirstChild(getgenv().FlingAllTarget).Character
+                if FlingEnemy and player.Character then
+                    FlingTorso = FlingEnemy.UpperTorso
+                    local dis = 3.85
+                    local increase = 6
+                    if FlingEnemy.Humanoid.MoveDirection.X < 0 then
+                        xchange = -increase
+                    elseif FlingEnemy.Humanoid.MoveDirection.X > 0  then
+                        xchange = increase
+                    else
+                        xchange = 0
+                    end
+                    if FlingEnemy.Humanoid.MoveDirection.Z < 0 then
+                        zchange = -increase
+                    elseif FlingEnemy.Humanoid.MoveDirection.Z > 0 then
+                        zchange = increase
+                    else
+                        zchange = 0
+                    end          
+                    if player.Character then
+						player.Character.Humanoid:ChangeState(11)
+                        player.Character.HumanoidRootPart.CFrame = CFrame.new(FlingTorso.Position.X + math.random(-dis,dis) + xchange, FlingTorso.Position.Y, FlingTorso.Position.Z + math.random(-dis,dis) + zchange) * CFrame.Angles(math.rad(player.Character.HumanoidRootPart.Orientation.X + 350),math.rad(player.Character.HumanoidRootPart.Orientation.Y + 200),math.rad(player.Character.HumanoidRootPart.Orientation.Z + 240))
+                        player.Character.HumanoidRootPart.Velocity = Vector3.new(500000,500000,500000)
+                    end
+                end
+            end)
+            if err then
+                print('fling error : ' .. err)
+            end
+        end;
+        local Start = function()    
+            OldFlingPos = player.Character.HumanoidRootPart.Position
+            Loop = game:GetService("RunService").Heartbeat:Connect(loopFunction);
+        end;
+        local Pause = function()
+            Loop:Disconnect()
+            local vectorZero = Vector3.new(0, 0, 0)
+            player.Character.PrimaryPart.Velocity = vectorZero
+            player.Character.PrimaryPart.RotVelocity = vectorZero
+            player.Character.HumanoidRootPart.CFrame = CFrame.new(OldFlingPos) * CFrame.Angles(math.rad(0),math.rad(137.92),math.rad(0))
+        end;
+        Start()	
+        repeat wait() until getgenv().FlingAll == false
+        Pause()
