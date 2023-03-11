9cf5d10841f8900e72d1bb11fbfd5a80e0a9c34e
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-10 21:56:09 -0500

    Update Fling.lua

--- Sources/Fling.lua
+++ Sources/Fling.lua
@@ -5,32 +5,32 @@ local Players = game.Players
         local loopFunction = function()
             local success,err = pcall(function()
                 if getgenv().FlingAll == true then
-                local FlingEnemy = Players:FindFirstChild(getgenv().FlingAllTarget).Character
-                if FlingEnemy and player.Character then
-                    FlingTorso = FlingEnemy.UpperTorso
-                    local dis = 3.85
-                    local increase = 6
-                    if FlingEnemy.Humanoid.MoveDirection.X < 0 then
-                        xchange = -increase
-                    elseif FlingEnemy.Humanoid.MoveDirection.X > 0  then
-                        xchange = increase
-                    else
-                        xchange = 0
+                    local FlingEnemy = Players:FindFirstChild(getgenv().FlingAllTarget).Character
+                    if FlingEnemy and player.Character then
+                        FlingTorso = FlingEnemy.UpperTorso
+                        local dis = 3.85
+                        local increase = 6
+                        if FlingEnemy.Humanoid.MoveDirection.X < 0 then
+                            xchange = -increase
+                        elseif FlingEnemy.Humanoid.MoveDirection.X > 0  then
+                            xchange = increase
+                        else
+                            xchange = 0
+                        end
+                        if FlingEnemy.Humanoid.MoveDirection.Z < 0 then
+                            zchange = -increase
+                        elseif FlingEnemy.Humanoid.MoveDirection.Z > 0 then
+                            zchange = increase
+                        else
+                            zchange = 0
+                        end          
+                game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = game.Players:FindFirstChild(getgenv().FlingAllTarget).Character.HumanoidRootPart	
+                        if player.Character then
+                            player.Character.Humanoid:ChangeState(11)
+                            player.Character.HumanoidRootPart.CFrame = CFrame.new(FlingTorso.Position.X + math.random(-dis,dis) + xchange, FlingTorso.Position.Y, FlingTorso.Position.Z + math.random(-dis,dis) + zchange) * CFrame.Angles(math.rad(player.Character.HumanoidRootPart.Orientation.X + 350),math.rad(player.Character.HumanoidRootPart.Orientation.Y + 200),math.rad(player.Character.HumanoidRootPart.Orientation.Z + 240))
+                            player.Character.HumanoidRootPart.Velocity = Vector3.new(500000,500000,500000)
+                        end
                     end
-                    if FlingEnemy.Humanoid.MoveDirection.Z < 0 then
-                        zchange = -increase
-                    elseif FlingEnemy.Humanoid.MoveDirection.Z > 0 then
-                        zchange = increase
-                    else
-                        zchange = 0
-                    end          
-			game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = game.Players:FindFirstChild(getgenv().FlingAllTarget).Character.HumanoidRootPart	
-                    if player.Character then
-						player.Character.Humanoid:ChangeState(11)
-                        player.Character.HumanoidRootPart.CFrame = CFrame.new(FlingTorso.Position.X + math.random(-dis,dis) + xchange, FlingTorso.Position.Y, FlingTorso.Position.Z + math.random(-dis,dis) + zchange) * CFrame.Angles(math.rad(player.Character.HumanoidRootPart.Orientation.X + 350),math.rad(player.Character.HumanoidRootPart.Orientation.Y + 200),math.rad(player.Character.HumanoidRootPart.Orientation.Z + 240))
-                        player.Character.HumanoidRootPart.Velocity = Vector3.new(500000,500000,500000)
-                    end
-                end
             end
             end)
             if err then
@@ -38,17 +38,7 @@ local Players = game.Players
             end
         end;
         local Start = function()    
-            OldFlingPos = player.Character.HumanoidRootPart.Position
-            Loop = game:GetService("RunService").Heartbeat:Connect(loopFunction);
+            getgenv().OldFlingPos = player.Character.HumanoidRootPart.Position
+            getgenv().Loop = game:GetService("RunService").Heartbeat:Connect(loopFunction);
         end;
-        local Pause = function()
-            Loop:Disconnect()
-            local vectorZero = Vector3.new(0, 0, 0)
-            player.Character.PrimaryPart.Velocity = vectorZero
-            player.Character.PrimaryPart.RotVelocity = vectorZero
-            player.Character.HumanoidRootPart.CFrame = CFrame.new(OldFlingPos) * CFrame.Angles(math.rad(0),math.rad(137.92),math.rad(0))
-        end;
-
         Start()	
-
---	game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = game.Players.LocalPlayer.Character:FindFirstChildWhichIsA('Humanoid')
