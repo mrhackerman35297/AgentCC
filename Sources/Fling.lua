1aaf5d1040633900d7f4764818eae69f861d0579
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-10 21:50:18 -0500

    Update Fling.lua

--- Sources/Fling.lua
+++ Sources/Fling.lua
@@ -4,6 +4,7 @@ local Players = game.Players
         local OldFlingPos = player.Character.HumanoidRootPart.Position
         local loopFunction = function()
             local success,err = pcall(function()
+                if getgenv().FlingAll == true then
                 local FlingEnemy = Players:FindFirstChild(getgenv().FlingAllTarget).Character
                 if FlingEnemy and player.Character then
                     FlingTorso = FlingEnemy.UpperTorso
@@ -30,6 +31,7 @@ local Players = game.Players
                         player.Character.HumanoidRootPart.Velocity = Vector3.new(500000,500000,500000)
                     end
                 end
+            end
             end)
             if err then
                 print('fling error : ' .. err)
@@ -48,7 +50,5 @@ local Players = game.Players
         end;
 
         Start()	
-repeat wait() until getgenv().FlingAll == false
 
-        Pause()
-	game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = game.Players.LocalPlayer.Character:FindFirstChildWhichIsA('Humanoid')
+--	game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = game.Players.LocalPlayer.Character:FindFirstChildWhichIsA('Humanoid')
