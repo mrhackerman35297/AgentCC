f35cc7baac46ac05026d54191419bd55d935c3b7
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-12 21:32:44 -0400

    Update Bypass.lua

--- Sources/Bypass.lua
+++ Sources/Bypass.lua
@@ -116,3 +116,22 @@ xpcall(function()
         end)
         setfflag("HumanoidParallelRemoveNoPhysics", "False")
         setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")      
+if not hookmetamethod then 
+		return notify('Incompatible Exploit','Your exploit does not support this command (missing hookmetamethod)')
+end
+Players = game:GetService("Players")
+	local LocalPlayer = Players.LocalPlayer
+	local oldhmmi
+	local oldhmmnc
+	oldhmmi = hookmetamethod(game, "__index", function(self, method)
+		if self == LocalPlayer and method:lower() == "kick" then
+			return error("Expected ':' not '.' calling member function Kick", 2)
+		end
+		return oldhmmi(self, method)
+	end)
+	oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
+		if self == LocalPlayer and getnamecallmethod():lower() == "kick" then
+			return
+		end
+		return oldhmmnc(self, ...)
+	end)
