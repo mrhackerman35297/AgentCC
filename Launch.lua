0186375d427088376ee43de85da69233c418b033
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-24 18:34:18 -0400

    Update Launch.lua

--- Launch.lua
+++ Launch.lua
@@ -1,3 +1,9 @@
+local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()--// [Free To Use Made By Vynixu]
+
+Inviter.Prompt({
+    name = "Agent Revamped",
+    invite = "https://discord.gg/wPQHasG3nX",
+})
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
 --[[if syn then
