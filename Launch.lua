bfd6704cec7ee9b4c38ac9fc5dbc6c6fc159048b
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-03-24 16:39:27 -0400

    Update Launch.lua

--- Launch.lua
+++ Launch.lua
@@ -1,25 +1,7 @@
 
-		local invite = 'wPQHasG3nX'
-		local http = game:GetService('HttpService')
-		local req = (syn and syn.request) or (http and http.request) or http_request
-		if req then
-			req({
-				Url = 'http://127.0.0.1:6463/rpc?v=1',
-				Method = 'POST',
-				Headers = {
-					['Content-Type'] = 'application/json',
-					Origin = 'https://discord.com'
-				},
-				Body = http:JSONEncode({
-					cmd = 'INVITE_BROWSER',
-					nonce = http:GenerateGUID(false),
-					args = {code = invite} 
-				})
-			})
-		end
-local Versions = "https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/"
-if syn then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+--[[if syn then
 loadstring(game:HttpGet(Versions..'GoodSploits'))()
 else
 loadstring(game:HttpGet(Versions..'BadSploits'))()
-end
+end]]
