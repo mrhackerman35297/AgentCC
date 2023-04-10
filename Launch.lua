609bd1657a3fbbdc7feec8b013e313cb0b72977a
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-04-10 10:02:57 -0400

    Update Launch.lua

--- Launch.lua
+++ Launch.lua
@@ -1,12 +1,53 @@
-local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Random.lua"))()
+if FLUXUS_LOADED then
+    	local invite = 'wPQHasG3nX'
+		local http = game:GetService('HttpService')
+		local req = (syn and syn.request) or (http and http.request) or http_request
+		if req then
+			req({
+				Url = 'http://127.0.0.1:6463/rpc?v=1',
+				Method = 'POST',
+				Headers = {
+					['Content-Type'] = 'application/json',
+					Origin = 'https://discord.com'
+				},
+				Body = http:JSONEncode({
+					cmd = 'INVITE_BROWSER',
+					nonce = http:GenerateGUID(false),
+					args = {code = invite} 
+				})
+			})
+		end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+elseif KRNL_LOADED then
+    	local invite = 'wPQHasG3nX'
+		local http = game:GetService('HttpService')
+		local req = (syn and syn.request) or (http and http.request) or http_request
+		if req then
+			req({
+				Url = 'http://127.0.0.1:6463/rpc?v=1',
+				Method = 'POST',
+				Headers = {
+					['Content-Type'] = 'application/json',
+					Origin = 'https://discord.com'
+				},
+				Body = http:JSONEncode({
+					cmd = 'INVITE_BROWSER',
+					nonce = http:GenerateGUID(false),
+					args = {code = invite} 
+				})
+			})
+		end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+else
+    local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Random.lua"))()
 
--- Creates an interactable prompt
-Inviter.Prompt({
-    name = "Agent Revamped",
-    invite = "https://discord.gg/wPQHasG3nX",
-    script = "https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits",
-})
-
+    -- Creates an interactable prompt
+    Inviter.Prompt({
+        name = "Agent Revamped",
+        invite = "https://discord.gg/wPQHasG3nX",
+        script = "https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits",
+    })
+end
 --// Old Shitty Dog Versions
 
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
