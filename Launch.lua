67e266d5403ff399663f57a058ebeb192d9195e3
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-04-10 10:20:50 -0400

    Update Launch.lua

--- Launch.lua
+++ Launch.lua
@@ -1,43 +1,11 @@
 if FLUXUS_LOADED then
-    	local invite = 'wPQHasG3nX'
-		local http = game:GetService('HttpService')
-		local req = (syn and syn.request) or (http and http.request) or http_request
-		if req then
-			req({
-				Url = 'http://127.0.0.1:6463/rpc?v=1',
-				Method = 'POST',
-				Headers = {
-					['Content-Type'] = 'application/json',
-					Origin = 'https://discord.com'
-				},
-				Body = http:JSONEncode({
-					cmd = 'INVITE_BROWSER',
-					nonce = http:GenerateGUID(false),
-					args = {code = invite} 
-				})
-			})
-		end
+    getgenv().Invite = 'wPQHasG3nX'
     loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Discord"))()
 elseif KRNL_LOADED then
-    	local invite = 'wPQHasG3nX'
-		local http = game:GetService('HttpService')
-		local req = (syn and syn.request) or (http and http.request) or http_request
-		if req then
-			req({
-				Url = 'http://127.0.0.1:6463/rpc?v=1',
-				Method = 'POST',
-				Headers = {
-					['Content-Type'] = 'application/json',
-					Origin = 'https://discord.com'
-				},
-				Body = http:JSONEncode({
-					cmd = 'INVITE_BROWSER',
-					nonce = http:GenerateGUID(false),
-					args = {code = invite} 
-				})
-			})
-		end
+       getgenv().Invite = 'wPQHasG3nX'
     loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Versions/BadSploits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Discord"))()
 else
     local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/mrhackerman35297/AgentCC/main/Sources/Random.lua"))()
 
